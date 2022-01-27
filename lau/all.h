abb86cc37f02d1dd1379d07a89ad8ad2b27dd9c4
Lau Yee-Yu <liuyiyu2003@126.com>
2022-01-27 17:43:17 +0800

    add: the header file for every data structures on disk storage (file_data_structure.h)

--- lau/all.h
+++ lau/all.h
@@ -1,10 +1,10 @@
 #ifndef LAU_CPP_LIB_LAU_ALL_H
 #define LAU_CPP_LIB_LAU_ALL_H
 
+#include "file_data_structure.h"
 #include "file_double_unrolled_linked_list.h"
 #include "file_unrolled_linked_list.h"
 #include "token_scanner.h"
 #include "utility.h"
-
 
 #endif //LAU_CPP_LIB_LAU_ALL_H
---
+++ lau/file_data_structure.h
@@ -0,0 +1,7 @@
+#ifndef LAU_CPP_LIB_LAU_FILE_DATA_STRUCTURE_H
+#define LAU_CPP_LIB_LAU_FILE_DATA_STRUCTURE_H
+
+#include "file_double_unrolled_linked_list.h"
+#include "file_unrolled_linked_list.h"
+
+#endif //LAU_CPP_LIB_LAU_FILE_DATA_STRUCTURE_H
