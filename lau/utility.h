06a9f84880101a5ec13b92960bb47e92191233ac
Lau Yee-Yu <liuyiyu2003@126.com>
2022-03-31 22:24:21 +0800

    add: pair in utility

--- lau/utility.h
+++ lau/utility.h
@@ -26,4 +26,29 @@
 
 #include "token_scanner.h"
 
+namespace lau {
+
+template<class T1, class T2>
+class pair {
+public:
+    constexpr pair() : first(), second() {}
+    pair(const pair &other) = default;
+    pair(pair &&other) = default;
+    pair(const T1 &x, const T2 &y) : first(x), second(y) {}
+
+    template<class U1, class U2>
+    pair(U1 &&x, U2 &&y) : first(x), second(y) {}
+
+    template<class U1, class U2>
+    pair(const pair<U1, U2> &other) : first(other.first), second(other.second) {}
+
+    template<class U1, class U2>
+    pair(pair<U1, U2> &&other) : first(other.first), second(other.second) {}
+
+    T1 first;
+    T2 second;
+};
+
+}
+
 #endif // LAU_CPP_LIB_LAU_UTILITY_H
