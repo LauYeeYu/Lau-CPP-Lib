b8f69b112036ff0eb338debe8d288d18d0b73e58
Lau Yee-Yu <liuyiyu2003@126.com>
2022-04-01 16:59:58 +0800

    refactor: the code in utility.h to fit code style

--- lau/utility.h
+++ lau/utility.h
@@ -29,21 +29,21 @@
 namespace lau {
 
 template<class T1, class T2>
-class pair {
+class Pair {
 public:
-    constexpr pair() : first(), second() {}
-    pair(const pair &other) = default;
-    pair(pair &&other) = default;
-    pair(const T1 &x, const T2 &y) : first(x), second(y) {}
+    Pair() : first(), second() {}
+    Pair(const Pair& other) = default;
+    Pair(Pair&& other) = default;
+    Pair(const T1& x, const T2& y) : first(x), second(y) {}
 
     template<class U1, class U2>
-    pair(U1 &&x, U2 &&y) : first(x), second(y) {}
+    Pair(U1&& x, U2&& y) : first(x), second(y) {}
 
     template<class U1, class U2>
-    pair(const pair<U1, U2> &other) : first(other.first), second(other.second) {}
+    Pair(const Pair<U1, U2>& other) : first(other.first), second(other.second) {}
 
     template<class U1, class U2>
-    pair(pair<U1, U2> &&other) : first(other.first), second(other.second) {}
+    Pair(Pair<U1, U2>&& other) : first(other.first), second(other.second) {}
 
     T1 first;
     T2 second;
